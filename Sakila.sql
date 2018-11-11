c8984e594d8ed2d641ef3edbb3bda170fe14246e
Hasan Sulaeman <hasanregius@gmail.com>
2018-11-10 17:10:12 -0800

    fixing some spacing

--- Sakila.sql
+++ Sakila.sql
@@ -10,38 +10,32 @@ FROM actor;
 
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. 
 -- 	     Name the column `Actor Name`
-SELECT CONCAT(first_name,  ' ', last_name) AS 'First Last'
-FROM actor;
+SELECT CONCAT(first_name,  ' ', last_name) AS 'First Last' FROM actor;
 
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, 
--- 	     "Joe." What is one query would you use to obtain this information?
-SELECT actor_id, first_name, last_name
-FROM actor
+--     "Joe." What is one query would you use to obtain this information?
+SELECT actor_id, first_name, last_name FROM actor
 WHERE first_name = 'Joe';
 
 -- 2b. Find all actors whose last name contain the letters `GEN`:
-SELECT actor_id, first_name, last_name
-FROM actor
+SELECT actor_id, first_name, last_name FROM actor
 WHERE last_name LIKE '%GEN%';
 
 -- 2c. Find all actors whose last names contain the letters `LI`. 
---       This time, order the rows by last name and first name, in that order:
-SELECT actor_id, first_name, last_name
-FROM actor
+--     This time, order the rows by last name and first name, in that order:
+SELECT actor_id, first_name, last_name FROM actor
 WHERE last_name LIKE '%LI%'
 ORDER BY last_name, first_name;
 
 -- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: 
--- 	     Afghanistan, Bangladesh, and China:
-SELECT country_id, country
-FROM country
+--     Afghanistan, Bangladesh, and China:
+SELECT country_id, country FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
 -- 3a. You want to keep a description of each actor. You don't think you will be performing 
---       queries on a description, so create a column in the table `actor` named `description` 
---       and use the data type `BLOB`.
-SELECT *
-FROM actor;
+--     queries on a description, so create a column in the table `actor` named `description` 
+--     and use the data type `BLOB`.
+SELECT * FROM actor;
 ALTER TABLE actor 
 ADD COLUMN actor_description BLOB AFTER last_name;
 
@@ -51,44 +45,41 @@ ALTER TABLE actor
 DROP COLUMN actor_description;
 
 -- 4a. List the last names of actors, as well as how many actors have that last name.
-SELECT last_name, COUNT(*)
-AS `Count` FROM actor GROUP BY last_name;
+SELECT last_name, COUNT(*) AS `Count` FROM actor GROUP BY last_name;
 
 -- 4b. List last names of actors and the number of actors who have that last name, 
 --       but only for names that are shared by at least two actors
-SELECT last_name, COUNT(*)
-AS `Count` FROM actor GROUP BY last_name
+SELECT last_name, COUNT(*) AS `Count` FROM actor 
+GROUP BY last_name
 HAVING Count > 2;
 
 -- 4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. 
 --     Write a query to fix the record.
 UPDATE actor 
-SET first_name='HARPO'
-WHERE first_name='GROUCHO' AND last_name='WILLIAMS';
+SET first_name='HARPO' WHERE first_name='GROUCHO' AND last_name='WILLIAMS';
 
 -- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` 
---       was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, 
---       change it to `GROUCHO`.
+--     was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, 
+--     change it to `GROUCHO`.
 UPDATE actor 
-SET first_name= 'GROUCHO'
-WHERE first_name='HARPO' AND last_name='WILLIAMS';
+SET first_name= 'GROUCHO' WHERE first_name='HARPO' AND last_name='WILLIAMS';
 
 -- 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?
 DESCRIBE sakila.address;
 
 -- 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. 
---       Use the tables `staff` and `address`:
+--     Use the tables `staff` and `address`:
 SELECT s.first_name, s.last_name, a.address
 FROM staff s LEFT JOIN address a ON s.address_id = a.address_id;
 
 -- 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. 
---       Use tables `staff` and `payment`.
+--     Use tables `staff` and `payment`.
 SELECT s.first_name, s.last_name, SUM(p.amount) AS 'TOTAL'
 FROM staff s LEFT JOIN payment p ON s.staff_id = p.staff_id
 GROUP BY s.first_name, s.last_name;
 
 -- 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` 
---       and `film`. Use inner join.
+--     and `film`. Use inner join.
 SELECT f.title, COUNT(a.actor_id) AS 'TOTAL'
 FROM film f LEFT JOIN film_actor a ON f.film_id = a.film_id
 GROUP BY f.title;
@@ -99,16 +90,15 @@ FROM film f INNER JOIN inventory i ON f.film_id = i.film_id
 WHERE title = "Hunchback Impossible";
 
 -- 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. 
---       List the customers alphabetically by last name:
+--     List the customers alphabetically by last name:
 SELECT c.first_name, c.last_name, SUM(p.amount) AS 'TOTAL'
 FROM customer c LEFT JOIN payment p ON c.customer_id = p.customer_id
 GROUP BY c.first_name, c.last_name
 ORDER BY c.last_name;
 
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, 
--- 	     films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries 
--- 	     to display the titles of movies starting with the letters `K` and `Q` whose language is English.
-USE sakila;
+--     films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries 
+--     to display the titles of movies starting with the letters `K` and `Q` whose language is English.
 SELECT title FROM film
 WHERE (title LIKE 'K%' OR title LIKE 'Q%')
 	AND language_id=(SELECT language_id
